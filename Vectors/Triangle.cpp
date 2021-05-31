ee155b6f8ed9173f45ceaa668f608aa2d45036cc
Georgi <jor4e_123@abv.bg>
2021-05-31 19:24:10 +0300

    Georgi Triangle Update

---
+++ Vectors/Triangle.cpp
@@ -0,0 +1,17 @@
+#include "Triangle.h"
+
+Triangle::Triangle()
+{
+	Point A(0, 0, 0);
+	Point B(0, 0, 0);
+	Point C(0, 0, 0);
+
+}
+
+Triangle::Triangle(Point A , Point B, Point B)
+{
+	Point A = this->Point A;
+	Point B = this->Point B;
+	Point C = this->Point C;
+
+}
